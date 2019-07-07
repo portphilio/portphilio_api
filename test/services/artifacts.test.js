688c96b786dfae81fb05f09f62524da3552e8fdf
Morgan Benton <morgan.benton@gmail.com>
2019-07-07 03:20:06 -0400

    eslint

--- test/services/artifacts.test.js
+++ test/services/artifacts.test.js
@@ -1,8 +1,8 @@
-const app = require('../../src/app');
+const app = require('../../src/app')
 
 describe('\'artifacts\' service', () => {
   it('registered the service', () => {
-    const service = app.service('artifacts');
-    expect(service).toBeTruthy();
-  });
-});
+    const service = app.service('artifacts')
+    expect(service).toBeTruthy()
+  })
+})
--- test/services/tokens.test.js
+++ test/services/tokens.test.js
@@ -1,8 +1,8 @@
-const app = require('../../src/app');
+const app = require('../../src/app')
 
 describe('\'tokens\' service', () => {
   it('registered the service', () => {
-    const service = app.service('tokens');
-    expect(service).toBeTruthy();
-  });
-});
+    const service = app.service('tokens')
+    expect(service).toBeTruthy()
+  })
+})
