b23a81bd5890c0bae6b6b429a59b49567c3d8ab0
afridizx <afridizx@dukes.jmu.edu>
2019-10-15 18:42:31 -0400

    add comment

--- test/services/tokens.test.js
+++ test/services/tokens.test.js
@@ -1,5 +1,6 @@
 const app = require('../../src/app')
 
+// test tokens.class.test.js
 describe('\'tokens\' service', () => {
   it('registered the service', () => {
     const service = app.service('tokens')
